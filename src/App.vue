5d2a75b236e131ea332f4949e1326655784804db
Feitosa <mathe.feitosa@gmail.com>
2021-11-21 10:24:08 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="flex items-center justify-center h-screen bg-gray-50">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from '@/components/Home'
+
+export default {
+  name: 'App',
+  metaInfo: {
+    title: 'Vue with Tailwind CSS',
+    titleTemplate: '%s | Template',
+    htmlAttrs: {
+      lang: 'pt-br',
+    }
+  },
+  components: {
+    Home
+  }
+}
+</script>
+
+<style lang="postcss">
+#app {
+  font-family: 'Montserrat', sans-serif;
+}
+</style>
