83f2402a1e819fd44f3b31de07206f032362b84a
Feitosa <mathe.feitosa@gmail.com>
2021-11-24 12:16:50 -0300

    Parcial finish of Homepage.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,49 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
+axios.defaults.baseURL = "https://boiling-oasis-59814.herokuapp.com/";
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    notes: [],
+    token:
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjYxOTJjNzRhMjQ2Y2E1MDgwMzU5ZDJlYiIsImV4cCI6MTYzNzc3MTczN30.uLlaULoP1fzISAbrakekEaLx-nYffa2zFT74moXsq9A",
+    email: null,
+    search: "",
+  },
+  mutations: {
+    setNotes(state, notes) {
+      state.notes = notes;
+    },
+    setEmail(state, email) {
+      state.email = email;
+    },
+    setSearch(state, search) {
+      state.search = search;
+    },
+  },
+  actions: {
+    getNotes({ commit }) {
+      axios
+        .post("/notes", {
+          token: this.state.token,
+        })
+        .then((response) => {
+          commit("setNotes", response.data);
+        });
+    },
+    login({ commit }, credentials) {
+      axios.post("/login", credentials).then((response) => {
+        commit("setToken", response.data.token);
+        commit("setEmail");
+      });
+    },
+    updateSearch({ commit }, search) {
+      commit("setSearch", search);
+    },
+  },
   modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <v-container>
+      <v-text-field
+        label="Search example: 11/16/2021"
+        solo
+        v-model="search"
+      ></v-text-field>
+      <v-row>
+        <v-col
+          cols="6"
+          sm="6"
+          md="4"
+          xl="2"
+          lg="4"
+          v-for="note in filteredNotes"
+          v-bind:key="note._id"
+        >
+          <v-card elevation="3" height="200">
+            <v-card-title>{{ note.date }}</v-card-title>
+            <v-card-subtitle><v-icon>mdi-home</v-icon></v-card-subtitle>
+            <v-card-text>{{ note.text }}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-btn fab large bottom right rounded fixed class="v-btn--example">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  name: "HelloWorld",
+  data() {
+    return {
+      search: "",
+    };
+  },
+  watch: {
+    search() {
+      this.$store.dispatch("updateSearch", this.search);
+    },
+  },
+  computed: {
+    notes() {
+      return this.$store.state.notes;
+    },
+    filteredNotes() {
+      return this.notes.filter((note) => {
+        return note.date.includes(this.$store.state.search);
+      });
+    },
+  },
+  mounted() {
+    this.$store.dispatch("getNotes");
   },
 };
 </script>
